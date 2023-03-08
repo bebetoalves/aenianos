4e858794ffd5f95e9a3d75835c0158cd416c7620
Bebeto <bebeto.tig@gmail.com>
2023-03-08 08:56:27 -0300

    add genre model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Faq;
+use App\Models\Genre;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,5 +18,6 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
         Faq::factory(10)->create();
         Post::factory(100)->create();
+        Genre::factory(10)->create();
     }
 }

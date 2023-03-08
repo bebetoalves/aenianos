0ad29724e43630e63748095f2092cd60b23fb0fd
Bebeto <bebeto.tig@gmail.com>
2023-03-08 11:11:21 -0300

    add return type for placekitten

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 if (! function_exists('placekitten')) {
-    function placekitten(int $width, int $height)
+    function placekitten(int $width, int $height): string
     {
         return sprintf('//placekitten.com/g/%d/%d', $width, $height);
     }

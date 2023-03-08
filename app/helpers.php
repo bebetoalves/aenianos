51131be6eb4670aa5fb8bc9618e538fd93936b18
Bebeto <bebeto.tig@gmail.com>
2023-03-08 16:01:18 -0300

    fix placekitten url

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,6 @@
 if (! function_exists('placekitten')) {
     function placekitten(int $width, int $height): string
     {
-        return sprintf('//placekitten.com/g/%d/%d', $width, $height);
+        return sprintf('https://placekitten.com/g/%d/%d', $width, $height);
     }
 }

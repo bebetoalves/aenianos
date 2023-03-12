a3aca4cde8426b26d611fb7643bde791b49a8787
Bebeto <bebeto.tig@gmail.com>
2023-03-12 17:41:15 -0300

    change default user name

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
+            'name' => 'Admin',
             'email' => 'admin@example.com',
             'role' => Role::ADMIN,
         ]);
